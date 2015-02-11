45172337f09b9b9a06e5a147e4b431b90edff38e
Ignacio Rossi <rossi.ignacio@gmail.com>
2015-02-11 20:50:22 -0300

    Debug Toolbar breaks on django 1.8 :( disabling for now

--- chorddb_django/chorddb_django/settings/local.py
+++ chorddb_django/chorddb_django/settings/local.py
@@ -47,18 +47,18 @@ CACHES = {
 ########## END CACHE CONFIGURATION
 
 
-########## TOOLBAR CONFIGURATION
-# See: http://django-debug-toolbar.readthedocs.org/en/latest/installation.html#explicit-setup
-INSTALLED_APPS += (
-    'debug_toolbar',
-)
-
-MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
-
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
-
-# http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
-INTERNAL_IPS = ('127.0.0.1',)
-########## END TOOLBAR CONFIGURATION
+# ########## TOOLBAR CONFIGURATION
+# # See: http://django-debug-toolbar.readthedocs.org/en/latest/installation.html#explicit-setup
+# INSTALLED_APPS += (
+#     'debug_toolbar',
+# )
+#
+# MIDDLEWARE_CLASSES += (
+#     'debug_toolbar.middleware.DebugToolbarMiddleware',
+# )
+#
+# DEBUG_TOOLBAR_PATCH_SETTINGS = False
+#
+# # http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
+# INTERNAL_IPS = ('127.0.0.1',)
+# ########## END TOOLBAR CONFIGURATION
