bb4209c00c5321526d877982eb3bb9491698e9a8
Ignacio Rossi <rossi.ignacio@gmail.com>
2015-02-15 17:38:09 -0300

    pylint: url files fixes

--- chorddb_django/song/urls.py
+++ chorddb_django/song/urls.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
+# pylint: disable=line-too-long
 from __future__ import absolute_import, unicode_literals
 
 import logging
@@ -12,7 +13,7 @@ from .views import SongListView, SongAddView, SongDetailView, HomeView
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 
-urlpatterns = patterns(
+urlpatterns = patterns(  # pylint: disable=invalid-name
     'song.views',
     url(r'^/?$', HomeView.as_view(), name='song_home'),
     url(r'^song/?$', SongListView.as_view(), name='song_song_list'),
