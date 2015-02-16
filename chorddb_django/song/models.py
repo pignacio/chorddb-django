48222f1e607cb51096ccf105c3a42de58afd06e8
Ignacio Rossi <rossi.ignacio@gmail.com>
2015-02-16 10:33:24 -0300

    Song and SongVersion get_absolute_url

--- chorddb_django/song/models.py
+++ chorddb_django/song/models.py
@@ -1,5 +1,6 @@
 from __future__ import  unicode_literals
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 class Song(models.Model):
@@ -10,12 +11,23 @@ class Song(models.Model):
     def __unicode__(self):
         return "Song: {s.artist} - {s.title}".format(s=self)
 
+    def get_absolute_url(self):
+        return reverse('song_song_detail', kwargs={'song_id': self.id})
+
 
 class SongVersion(models.Model):
     song = models.ForeignKey('Song')
     instrument = models.ForeignKey('InstrumentModel')
 #      chord_versions = models.TextField() # Json or HStore
 #      special_chords = models.TextField() # Json or Array<HStore>
+
+    def __unicode__(self):
+        return 'SongVersion: {s.song.title} for {s.instrument.name}'.format(
+            s=self)
+
+    def get_absolute_url(self):
+        return reverse('song_songversion_detail',
+                       kwargs={'songversion_id': self.id})
 
 
 class InstrumentModel(models.Model):
