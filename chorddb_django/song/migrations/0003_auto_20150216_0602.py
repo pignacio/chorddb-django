6c24cda106a39df396e3513ea5906285dd6405a8
Ignacio Rossi <rossi.ignacio@gmail.com>
2015-02-16 11:03:33 -0300

    Add capo and transpose to SongVersion

---
+++ chorddb_django/song/migrations/0003_auto_20150216_0602.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('song', '0002_basic_instruments'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='songversion',
+            name='capo',
+            field=models.PositiveIntegerField(default=0),
+        ),
+        migrations.AddField(
+            model_name='songversion',
+            name='transpose',
+            field=models.IntegerField(default=0),
+        ),
+    ]
